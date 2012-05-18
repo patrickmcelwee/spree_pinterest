5dbf3885f95cd28b7c8447866830d0548cead840
Patrick McElwee <pmcelwee@gmail.com>
2012-05-18 11:35:55 -0400

    Fixed styling and eliminated from products index page

--- app/overrides/pin_it_button.rb
+++ app/overrides/pin_it_button.rb
@@ -1,7 +1,8 @@
 Deface::Override.new(:virtual_path => "shared/_products",
                      :insert_bottom => "[data-hook='products_list_item']",
                      :text => "<%= pin_it_button(product) %>",
-                     :name => "index_pin_it")
+                     :name => "index_pin_it",
+		     :disabled => true)
 
 Deface::Override.new(:virtual_path => "products/show",
                      :insert_after => "#main-image",
