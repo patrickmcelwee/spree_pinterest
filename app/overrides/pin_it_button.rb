47cccf8c45cdf0dfa950ed5faafb20402a671427
Patrick McElwee <pmcelwee@gmail.com>
2012-05-18 11:20:19 -0400

    Removed additional namespacing for reversion

--- app/overrides/pin_it_button.rb
+++ app/overrides/pin_it_button.rb
@@ -1,9 +1,9 @@
-Deface::Override.new(:virtual_path => "spree/shared/_products",
+Deface::Override.new(:virtual_path => "shared/_products",
                      :insert_bottom => "[data-hook='products_list_item']",
                      :text => "<%= pin_it_button(product) %>",
                      :name => "index_pin_it")
 
-Deface::Override.new(:virtual_path => "spree/products/show",
+Deface::Override.new(:virtual_path => "products/show",
                      :insert_after => "#main-image",
                      :text => "<%= pin_it_button(@product) %>",
                      :name => "show_pin_it")
