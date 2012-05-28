682ff5a07a977c8ae65cc9c9115b8de66c40cff9
Patrick McElwee <pmcelwee@gmail.com>
2012-05-28 15:10:29 -0400

    Namespaced overrides

--- app/overrides/pin_it_button.rb
+++ app/overrides/pin_it_button.rb
@@ -1,10 +1,10 @@
-Deface::Override.new(:virtual_path => "shared/_products",
+Deface::Override.new(:virtual_path => "spree/shared/_products",
                      :insert_bottom => "[data-hook='products_list_item']",
                      :text => "<%= pin_it_button(product) %>",
                      :name => "index_pin_it",
 		     :disabled => true)
 
-Deface::Override.new(:virtual_path => "products/show",
+Deface::Override.new(:virtual_path => "spree/products/show",
                      :insert_after => "#thumbnails",
                      :text => %q{<div id="pin-it" data-url="<%= product_url(@product) %>" data-description="<%= @product.name %>">
 			     	<%= pin_it_button(@product) %>
